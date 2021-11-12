90ff8bd279af87561beb0a385ccdb1344e485619
Mike Takano <mikematsuri@gmail.com>
2021-11-12 22:50:16 +0900

    Add converter for ics to csv format

---
+++ convert_csv.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from csv_ical import Convert
+
+convert = Convert()
+convert.SAVE_LOCATION = 'foursquare.ics'
+convert.CSV_FILE_LOCATION = 'foursquare.csv'
+
+convert.read_ical(convert.SAVE_LOCATION)
+convert.make_csv()
+convert.save_csv(convert.CSV_FILE_LOCATION)
+
